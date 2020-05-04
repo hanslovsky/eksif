28963b0b6ad42a49b3bf60cbc41c77e7b4de1500
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2020-05-03 20:05:36 -0400

    Add bash completion subcommand

--- src/main/kotlin/me/hanslovsky/eksif/Eksif.kt
+++ src/main/kotlin/me/hanslovsky/eksif/Eksif.kt
@@ -3,16 +3,20 @@
  */
 package me.hanslovsky.eksif
 
+import picocli.AutoComplete
 import picocli.CommandLine
 import java.util.concurrent.Callable
 import kotlin.system.exitProcess
 
-@CommandLine.Command(name = "eksif", subcommands = [PrintExif::class])
+@CommandLine.Command(
+        name = "eksif",
+        subcommands = [PrintExif::class, AutoComplete.GenerateCompletion::class])
 class Eksif: Callable<Int> {
 
-    override fun call(): Int {
-        return 0
-    }
+    @CommandLine.Option(names = ["--help", "-h"], required = false, usageHelp = true, description = ["Print this message."])
+    var showUsageHelp = false
+
+    override fun call(): Int = 0
 
     companion object {
         @JvmStatic
